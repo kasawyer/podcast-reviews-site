692b69dd83a295b3a78c38fff487525f182dc72c
Rahul Krishnan <rk2211@gmail.com>
2017-01-19 10:20:15 -0500

    Added tests for devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :podcasts
-  
+
   validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/ }
   validates :password, presence: true
 end
