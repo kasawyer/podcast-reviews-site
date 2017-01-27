5611ef2bb9680b94f8c14aa3469953f30ccf3b65
Marjie Lam <marjielam@gmail.com>
2017-01-26 19:10:29 -0500

    finished enzyme testing for podcast index page

--- react/test/userSeesPodcastsSpec.js
+++ react/test/userSeesPodcastsSpec.js
@@ -1,12 +1,20 @@
 import PodcastList from '../src/components/PodcastList';
 
 describe('Podcast List', () => {
-  var wrapper;
+  let wrapper;
 
   beforeEach(() => {
-    spyOn(global, 'fetch').and.returnValue(
-      createResponseFromFixture('podcasts/podcastsIndex')
-    );
+    spyOn(global, 'fetch').and.callFake(url => {
+      if (url.endsWith('/api/v1/podcasts.json')) {
+        return createResponseFromFixture('podcasts/podcastsIndex');
+      } else if (url.endsWith('/api/v1/podcasts/4/providers.json')) {
+        return createResponseFromFixture('providers/providersIndex_1');
+      } else if (url.endsWith('/api/v1/podcasts/1/providers.json')) {
+        return createResponseFromFixture('providers/providersIndex_1');
+      } else if (url.endsWith('/api/v1/podcasts/2/providers.json')) {
+        return createResponseFromFixture('providers/providersIndex_2');
+      }
+    });
   });
 
   afterEach(() => {
@@ -20,13 +28,25 @@ describe('Podcast List', () => {
       );
     });
 
-    it('lists al the podcasts', done => {
+    it('lists all the podcast names', done => {
       setTimeout(() => {
         let pageText = wrapper.text();
+
         expect(pageText).toMatch('This American Life');
         expect(pageText).toMatch('Serial');
         expect(pageText).toMatch('Hardcore History');
 
+        done();
+      }, 0);
+    });
+
+    it('lists the providers under the names of the podcasts', done => {
+      setTimeout(() => {
+        let pageText = wrapper.text();
+
+        expect(pageText).toMatch('NPR');
+        expect(pageText).toMatch('HHC.com');
+        
         done();
       }, 0);
     });
